5b5182b9da52d03eebfb25b74bd2354b57657089
dio <dioputrautama@gmail.com>
2025-02-26 17:56:54 +0700

    fix(OpenCVWrapper): Handle orientation Image

--- ImageTemperature/OpenCVWrapper.mm
+++ ImageTemperature/OpenCVWrapper.mm
@@ -6,25 +6,46 @@
 //
 
 #import "OpenCVWrapper.h"
+#import <opencv2/opencv.hpp>
 #import <opencv2/imgcodecs/ios.h>
 
 @implementation OpenCVWrapper
 
++ (UIImage *)normalizeImageOrientation:(UIImage *)image {
+    if (image.imageOrientation == UIImageOrientationUp) {
+        return image;
+    }
+
+    UIGraphicsBeginImageContextWithOptions(image.size, NO, image.scale);
+    [image drawInRect:CGRectMake(0, 0, image.size.width, image.size.height)];
+    UIImage *normalizedImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    return normalizedImage;
+}
+
 + (UIImage *)adjustTemperature:(UIImage *)image temperature:(int)temperature {
+
+    UIImage *normalizedImage = [self normalizeImageOrientation:image];
+
     cv::Mat mat;
 
-    // Konversi UIImage ke OpenCV Mat
-    UIImageToMat(image, mat);
+    UIImageToMat(normalizedImage, mat);
 
     std::vector<cv::Mat> channels;
     cv::split(mat, channels);
 
+    // BGR: Blue (0), Green (1), Red (2)
     if (temperature > 0) {
-        channels[2] += temperature; // Tambah merah (R)
-        channels[0] -= temperature; // Kurangi biru (B)
+        // Warm transition: First yellow (increase R & G), then red
+        channels[2] += temperature; // Increase Red
+        channels[1] += temperature * 0.5; // Increase Green (Yellow effect)
+        channels[0] -= temperature * 0.3; // Reduce Blue slightly
     } else {
-        channels[2] -= abs(temperature); // Kurangi merah (R)
-        channels[0] += abs(temperature); // Tambah biru (B)
+        // Cool transition: First light blue (increase B & G), then deep blue
+        channels[0] += abs(temperature); // Increase Blue
+        channels[1] += abs(temperature) * 0.5; // Increase Green (Light blue effect)
+        channels[2] -= abs(temperature) * 0.3; // Reduce Red slightly
     }
 
     cv::merge(channels, mat);
@@ -33,3 +54,5 @@
 }
 
 @end
+
+
